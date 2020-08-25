0617e7057fc9c09e8f106936d7532ee3d7c69879
Pratik Prasad Devkota <pratikdevkota82@gmail.com>
2020-08-25 17:08:49 +0545

    Update Problem4.py
    Included the initial question.

--- Problem4.py
+++ Problem4.py
@@ -1,11 +1,19 @@
+"""A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers."""
+
 import time
+
 start_time = time.time()
 palindromes = []
-for i in range(1000, 10000):
-    for j in range(1000, 10000):
+
+for i in range(100, 1000):
+    for j in range(100, 1000):
         k = i * j
         if str(k) == str(k)[::-1]:
             palindromes.append(k)
+            
 palindromes.sort()
+
 print(palindromes[-1])
 print(f"Program executed in {time.time() - start_time}s.")
